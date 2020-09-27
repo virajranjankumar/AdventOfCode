d970bab168fd20db87c8d50eddf347ee93748013
Viraj Ranjankumar <virajranjankumar@Virajs-MacBook-Pro.local>
2020-09-27 00:32:14 -0500

    feat: create layer and compute

---
+++ day8-1.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const getLayers = (line, width, height) => {
+  const layerLength = width * height;
+  const layers = new Array(Math.round(line.length / layerLength));
+  for (let i = 0; i < layers.length; i++) {
+    const start = i * layerLength;
+    layers[i] = line.slice(start, start + layerLength);
+  }
+  return layers;
+};
+
+const indexOfSmallest = (a) => a.indexOf(Math.min.apply(Math, a));
+
+const compute = (line, width, height) => {
+  const layers = getLayers(line, width, height);
+
+  const lengths = layers.map((layer) => (layer.match(/0/g) || []).length);
+  const smallestIndex = indexOfSmallest(lengths);
+  const layer = layers[smallestIndex];
+  const ones = (layer.match(/1/g) || []).length;
+  const twos = (layer.match(/2/g) || []).length;
+  return ones * twos;
+};
+
+function main(inputFile = "./input/day8.txt") {
+  const text = fs.readFileSync(inputFile, "utf-8");
+  return compute(text, 25, 6);
+}
+
+console.log(main());
+// Answer: 1320
